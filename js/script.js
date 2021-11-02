3ec8ea35c1ed90610b1f5d652395cc3ecd51fd9a
GinoGreen <luigiverde97@gmail.com>
2021-11-02 16:53:02 +0100

    commit routine

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//seleziono il mio container della griglia
+const myContainer = document.querySelector('.myContainer');
+//creo un array di numeri per riempire gli square
+const listNumbers = [];
+//memorizzo il value dell'option di livello di difficoltá
+let valueDifficult = document.querySelector('');
+
+
+//genero la griglia una volta cliccato il bottone PLAY
+const playButton = document.querySelector('header .play button.btn_custom');
+
+playButton.addEventListener('click', function () {
+   
+   initGrid();
+
+});
+
+function initGrid() {
+   
+   //cicolo che crea TOT quadrati dipendentemente dalla difficoltá scelta
+   for (let i = 0; i < 100; i++) {
+      
+      //creo l'elemento square e lo aggiungo dentro al container
+      const square = createSquare(myContainer);
+      //aggiungo la classe allo square
+      square.className = ('square easy');
+      //inserisco il numbero indice+1 dentro lo square
+      square.innerHTML = (i+1);
+      console.log('Square numero ', square.innerHTML);
+      
+   }
+}
+
+//funzione che crea quadrati 'square' e li inserire nel container (target)
+function createSquare(target) {
+   
+   const square = document.createElement('div');
+   target.append(square);
+
+   return square;
+}
+
